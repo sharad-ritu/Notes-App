e1ef7a975c1210f1a59b05cb63bc31c0396b6004
Sharad <sharadritu.58.69@gmail.com>
2024-01-17 22:34:56 +0545

    Setting up app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+
+const app = express();
+const port = 5000 || process.env.PORT;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//static files
+app.use(express.static('public'));
+
+//templating engine
+app.use(expressLayouts);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
