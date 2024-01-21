ab0fe130fda92301c25f01acfd2bb77a9230c30b
Sharad <sharadritu.58.69@gmail.com>
2024-01-21 13:59:23 +0545

    changed port to3000

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 
 const app = express();
-const port = 5000 || process.env.PORT;
+const port = 3000 || process.env.PORT;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -18,7 +18,11 @@ app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('index');
+    const locals = {
+        title: 'Notes App',
+        description: 'NodeJs Notes App'
+    }
+    res.render('index', locals);
 });
 
 app.listen(port, () => {
