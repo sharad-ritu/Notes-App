32323d308427d1846c67c976234aa89777da6219
Sharad <sharadritu.58.69@gmail.com>
2024-02-29 23:08:55 +0545

    Update dashboardController.js

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -1,12 +1,13 @@
-//get dashboard
-exports.dashboard = async(req, res) => {
-    const locals = {
-        title: 'Dashboard',
-        description: 'NodeJs Notes App'
-    }
 
-    res.render('dashboard/index', {
-        locals,
-        layout: './layouts/dashboard'
-    });
+//get dashboard
+exports.dashboard = async (req, res) => {
+  const locals = {
+    title: 'Dashboard',
+    description: 'NodeJs Notes App',
+  };
+
+  res.render('dashboard/index', {
+    locals,
+    layout: './layouts/dashboard',
+  });
 };
