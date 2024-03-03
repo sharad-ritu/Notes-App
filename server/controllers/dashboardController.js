c2fadb15326d86bacaedead1ff5e303f0be669c5
Sharad <sharadritu.58.69@gmail.com>
2024-03-03 20:32:00 +0545

    Update dashboardController.js

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -2,6 +2,8 @@ const Note = require('../models/notes');
 const mongoose = require('mongoose');
 //get dashboard
 exports.dashboard = async (req, res) => {
+  let perPage = 12;
+  let page = req.query.page || 1;
   
   const locals = {
     title: 'Dashboard',
@@ -9,13 +11,29 @@ exports.dashboard = async (req, res) => {
   };
 
   try {
-    const notes = await Note.find({});
+    const notes = await Note.aggregate([
+      { $sort: { updatedAt: -1 } },
+      { $match: { user: req.user.id } },
+      {
+        $project: {
+          title: { $substr: ["$title", 0, 30] },
+          body: { $substr: ["$body", 0, 100] },
+        },
+      }
+      ])
+    .skip(perPage * page - perPage)
+    .limit(perPage)
+    .exec(); 
+
+    const count = await Note.countDocuments({ user: req.user.id });
 
     res.render('dashboard/index', {
       userName: req.user.firstName,
       notes,
       locals,
       layout: './layouts/dashboard',
+      current: page,
+      pages: Math.ceil(count / perPage),
     });
   } catch (error) {
     console.log(error);
